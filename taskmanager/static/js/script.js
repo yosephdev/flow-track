991c8b33d78cb47919ccdadaaf6371f0e614c20d
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-25 12:44:15 +0000

    feat: Add JavaScript functions to initialize Materialize components

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,10 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-  // sidenav initialization
-  let sidenav = document.querySelectorAll(".sidenav");
+document.addEventListener("DOMContentLoaded", () => {
+  initSidenav();
+  initDatepicker();
+  initSelect();
+});
+
+const initSidenav = () => {
+  const sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav, {
     edge: 'left',
     inDuration: 250,
     outDuration: 200,
     draggable: true
   });
-});
+};
+
+const initDatepicker = () => {
+  const datepicker = document.querySelectorAll(".datepicker");
+  const {
+    Datepicker
+  } = M;
+  Datepicker.init(datepicker, {
+    format: "dd mmmm, yyyy",
+    i18n: {
+      done: "Select"
+    }
+  });
+};
+
+const initSelect = () => {
+  const selects = document.querySelectorAll("select");
+  const {
+    FormSelect
+  } = M;
+  FormSelect.init(selects);
+};
