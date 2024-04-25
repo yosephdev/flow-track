5bcb0803b296dc1c37fa8c7066bffd54cef1797d
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-25 11:28:28 +0000

    feat: Add a route to handle the deletion of categories

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -55,3 +55,11 @@ def edit_category(category_id):
         flash("Category Updated Successfully")
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
+
+@app.route("/delete_category/<int:category_id>")
+def delete_category(category_id):
+    category = Category.query.get_or_404(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    flash("Category Deleted Successfully")
+    return redirect(url_for("categories"))
