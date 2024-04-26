541df998100d8c6ab8d947cdf63a48f702e26a4b
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-26 12:50:41 +0000

    feat: Configure database URI dynamically in __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,12 +1,21 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
+app.config.from_object(Config)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
