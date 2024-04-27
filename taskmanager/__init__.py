799a8c20ecbae9db75078bb1024d21a579e95c6a
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-27 13:15:22 +0000

    feat: Add the Flask application context to init.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
-db = SQLAlchemy(app)
+with app.app_context():
+    db = SQLAlchemy(app)
 
 from taskmanager import routes
