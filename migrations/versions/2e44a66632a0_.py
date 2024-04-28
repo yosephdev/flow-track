f1b04ad39bafc27bf2b1a7455c9b609a458b20fe
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-28 14:20:05 +0000

    fix: Delete two migration files

---
+++ migrations/versions/2e44a66632a0_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2e44a66632a0
+Revises: cf9e874b62c6
+Create Date: 2024-04-28 14:00:17.816740
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2e44a66632a0'
+down_revision = 'cf9e874b62c6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
