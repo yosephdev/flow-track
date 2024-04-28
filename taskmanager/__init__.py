5a8dcfb44a241814016f06aedd1b4b04d5f39e07
Yoseph Gebremedhin <yosephbet@gmail.com>
2024-04-28 14:18:05 +0000

    fix: Add SQLAlchemy migration setup to init.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -12,8 +13,10 @@ app = Flask(__name__)
 
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 with app.app_context():
-    db = SQLAlchemy(app)
+  db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from taskmanager import routes
+from taskmanager import routes, models
